773eab0d3e2ff7a9d61774fbad62aa2320cb486e
Diogo Dores <diogo.dores@gmail.com>
2017-10-03 11:00:54 +0100

    primitives

--- LAIG/Lab1/MyGraphLeaf.js
+++ LAIG/Lab1/MyGraphLeaf.js
@@ -13,26 +13,22 @@ function MyGraphLeaf(graph, xmlelem) {
 }
 
 MyGraphLeaf.prototype.parseLeafs = function(){
-    ///witch(this.type){
-        //case triangle:
-            this.primitive = new MyTriangle(this.graph.scene, 0,0, 0, 1,0,0, 0,1,0);
-    //}
-  /*
+
     switch(this.type){
         case "rectangle":
-        this.rectangle = new MyQuad(this.graph.scene, 0, 1, 0, 1);
+        this.primitive = new MyQuad(this.graph.scene, 0, 1, 0, 1);
         break;
         case "cylinder":
-        this.cylinder = new MyCylinder(this.graph.scene, 12, 20, true);
+        this.primitive = new MyCylinder(this.graph.scene, 12, 20, true);
         break;
         case "sphere":
         console.log("No sphere available");
         break;
         case "triangle":
-        this.triangle = new MyTriangle(this.graph.scene, 0, 0, 0, 0, 0, 0, 0, 0, 0); //shit ton de zeros = coordenadas
+        this.primitive = new MyTriangle(this.graph.scene, 0, 0, 0, 1, 0, 0, 0, 1, 0);
         break;
     }
-    */
+
 }
 
 MyGraphLeaf.prototype.display = function(){
