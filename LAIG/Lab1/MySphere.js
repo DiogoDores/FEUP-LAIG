5b3b5fc828a0eeb958cfc451b21fde809b9f649f
João Seixas <up201505648@fe.up.pt>
2017-10-04 17:42:09 +0100

    I think now its done ma boi

--- LAIG/Lab1/MySphere.js
+++ LAIG/Lab1/MySphere.js
@@ -5,47 +5,24 @@
 function MySphere(scene, args) {
     CGFobject.call(this, scene);
 
-    this.radius = args[0]
-    this.slices = args[1];
-    this.stacks = args[2];
+    this.semi = new MySemisphere(scene, args);
 
-    this.initBuffers();
 }
 ;MySphere.prototype = Object.create(CGFobject.prototype);
 MySphere.prototype.constructor = MySphere;
 
-MySphere.prototype.initBuffers = function() {
+MySphere.prototype.display = function() {
 
-    this.vertices = [];
-    this.indices = [];
-    this.normals = [];
-    this.texCoords = [];
+    this.semi.display();
 
-    var ang = Math.PI * 2 / this.slices;
-    var angEsf = Math.PI / this.stacks;
+    this.scene.rotate(Math.PI,1,0,0);
+    this.semi.display();
 
-    for (j = parseInt(-this.stacks/2 -0.5) ; j < this.stacks/2 + 1; j++) {
-        for (i = 0; i < this.slices; i++) {
+};
 
-            this.vertices.push(Math.cos(i * ang) * Math.cos(angEsf * j) * this.radius, Math.sin(i * ang) * Math.cos(angEsf * j) * this.radius, Math.sin(angEsf * j) * this.radius);
-            this.normals.push(Math.cos(i * ang) * Math.cos(angEsf * j), Math.sin(i * ang) * Math.cos(angEsf * j), Math.sin(angEsf * j));
-            this.texCoords.push((Math.cos(i * ang) * Math.cos(angEsf * j) + 1)/2, (Math.sin(i * ang) * Math.cos(angEsf * j) + 1)/2 );
-        }
-    }
+function MyUnitCubeQuad(scene) {
+	CGFobject.call(this,scene);
 
-    for (j = 0; j < this.stacks; j++) {
-        for (i = 0; i < this.slices; i++) {
-            if (i == this.slices - 1) {
-                this.indices.push(0 + this.slices * j, 0 + this.slices * (j + 1), 0 + i + this.slices * (j + 1));
-                this.indices.push(0 + this.slices * j, 0 + i + this.slices * (j + 1), 0 + i + this.slices * j);
-            } else {
-                this.indices.push(0 + i + this.slices * j, 1 + i + this.slices * j, 0 + i + this.slices * (j + 1));
-                this.indices.push(1 + i + this.slices * j, 1 + i + this.slices * (j + 1), 0 + i + this.slices * (j + 1));
-            }
-        }
-    }
+    this.quad = new MyQuad(this.scene);
 
-    this.primitiveType = this.scene.gl.TRIANGLES;
-    this.initGLBuffers();
-}
-;
+};
