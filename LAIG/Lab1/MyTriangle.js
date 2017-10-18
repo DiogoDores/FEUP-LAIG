78ab08d74d85f853bae7a5fa9da4f421db920e23
Diogo Dores <diogo.dores@gmail.com>
2017-10-18 10:31:39 +0100

    Textures working

--- LAIG/Lab1/MyTriangle.js
+++ LAIG/Lab1/MyTriangle.js
@@ -46,27 +46,27 @@
      this.t = texture[2];
    }
 
-   var a = Math.sqrt(	Math.pow((this.x1 - this.x3),2) +
-    					Math.pow((this.y1 - this.y3),2) +
-    					Math.pow((this.z1 - this.z3),2));
+   var a = Math.sqrt(	Math.pow((this.x2 - this.x3),2) +
+    					Math.pow((this.y2 - this.y3),2) +
+    					Math.pow((this.z2 - this.z3),2));
 
-   var b = Math.sqrt(	Math.pow((this.x2 - this.x1),2) +
-    					Math.pow((this.y2 - this.y1),2) +
-    					Math.pow((this.z2 - this.z1),2));
+   var b = Math.sqrt(	Math.pow((this.x3 - this.x1),2) +
+    					Math.pow((this.y3 - this.y1),2) +
+    					Math.pow((this.z3 - this.z1),2));
 
-    var c = Math.sqrt(	Math.pow((this.x3 - this.x2),2) +
-    					Math.pow((this.y3 - this.y2),2) +
-    					Math.pow((this.z3 - this.z2),2));
+    var c = Math.sqrt(	Math.pow((this.x1 - this.x2),2) +
+    					Math.pow((this.y1 - this.y2),2) +
+    					Math.pow((this.z1 - this.z2),2));
 
 	var beta   = Math.acos(((a * a) - (b * b) + (c * c))/(2 * a * c));
 
 	var height = a * Math.sin(beta);
-  var mid = c - a * Math.cos(beta);
+  var base = c - a * Math.cos(beta);
 
     this.texCoords = [
-    mid/this.s, (1 - height)/this.t,
-		0, 1/this.t,
-		c/this.s, 1/this.t
+      0, height/this.t,
+      c/this.s, height/this.t,
+      base/this.s, (height - height)/this.t
     ];
 
 	this.updateTexCoordsGLBuffers();
